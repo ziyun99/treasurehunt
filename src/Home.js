df81979e93d0359ef2d7b632d141d929f1d5e83e
Yang Zi Yun <zy@Yangs-MacBook-Air.local>
2025-04-24 04:35:09 +0800

    fix react env var

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,15 @@ import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, signO
 import { getFirestore, doc, getDoc, setDoc, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_VITE_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_VITE_FIREBASE_APP_ID
 };
 
-console.log("🔥 Firebase Config:", firebaseConfig);
-
+console.log("🔥 Firebase Config:", firebaseConfig); 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
